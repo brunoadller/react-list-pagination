ba7e4f5352049f304cff338c6b424906469f0d3c
brunoadller <adller676@gmail.com>
2024-02-21 11:49:07 -0300

    react na pratica

---
+++ src/hooks/use-debounce-value.ts
@@ -0,0 +1,21 @@
+
+//utilizando para realizar um ação do input quando parar de digitar
+import {useEffect, useState} from 'react'
+
+export default function useDebounceValue<T = unknown>(value: T, delay: number){
+
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+
+    useEffect(()=> {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+
+    return debouncedValue
+}
